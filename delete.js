69634752556749f1ce0c9b95f676dcee4c18487e
EC2 Default User <ec2-user@ip-172-31-33-252.ec2.internal>
2020-04-10 21:07:31 +0000

    fixed error logging

--- delete.js
+++ delete.js
@@ -1,19 +1,19 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
-export async function main(event, context) {
-    const params = {
-        TableName: process.env.tableName,
-        Key: {
-            userId: event.requestContext.identity.cognitoIdentityId,
-            noteId: event.pathParameters.id
-        }
-    };
-    try {
-        await dynamoDbLib.call("delete", params);
-        return success({status: true});
-    } catch (e) {
-        console.log(e);
-        return failure({event: e, status: false});
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be removed
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
     }
-}
+  };
+
+  await dynamoDb.delete(params);
+
+  return { status: true };
+});
