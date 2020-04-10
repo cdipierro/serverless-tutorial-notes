1e7e1ed5eecbc479e492d36b737f93f7e9b6b7a6
EC2 Default User <ec2-user@ip-172-31-33-252.ec2.internal>
2020-04-10 17:12:08 +0000

    Setting up Serverless infrastructure

--- delete.js
+++ delete.js
@@ -13,6 +13,7 @@ export async function main(event, context) {
         await dynamoDbLib.call("delete", params);
         return success({status: true});
     } catch (e) {
-        return failure({status: false});
+        console.log(e);
+        return failure({event: e, status: false});
     }
 }
